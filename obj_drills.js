db5197673a294351444978043cb535d3b3e2130f
lilmaifun <lilmaifun@gmail.com>
2019-09-18 08:51:34 -0700

    object creator

---
+++ obj_drills.js
@@ -0,0 +1,11 @@
+//object creator
+function createMyObject(){
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function(){
+            return 'hello';
+        }
+    };
+}
