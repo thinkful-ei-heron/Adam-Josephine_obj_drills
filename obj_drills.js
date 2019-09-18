009761af90f6d9cd233cd46dbdf717708731f7e7
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-18 12:04:57 -0400

    keyDeleter

--- obj_drills.js
+++ obj_drills.js
@@ -1,16 +1,16 @@
 //object creator
-function createMyObject(){
+function createMyObject() {
     return {
         foo: 'bar',
         answerToUniverse: 42,
         'olly olly': 'oxen free',
-        sayHello: function(){
+        sayHello: function () {
             return 'hello';
         }
     };
 }
 
-
+//object updater
 function updateObject(obj) {
     obj.foo = 'foo';
     obj.bar = 'bar';
@@ -19,15 +19,30 @@ function updateObject(obj) {
     return obj;
 }
 
+//person maker
 function personMaker() {
     var person = {
-      firstName: 'Paul',
-      lastName: 'Jones',
-      // replace `null` with a function that uses self reference to return
-      // full name
-      fullName: function(){
-        return `${this.firstName} ${this.lastName}`
-      },
+        firstName: 'Paul',
+        lastName: 'Jones',
+        // replace `null` with a function that uses self reference to return
+        // full name
+        fullName: function () {
+            return `${this.firstName} ${this.lastName}`
+        },
     };
     return person;
-  }
+}
+
+//delete keys
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+};
+
+function keyDeleter(obj) {
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+}
