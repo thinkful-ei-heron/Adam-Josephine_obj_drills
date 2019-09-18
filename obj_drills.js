571def7c278ccfc76daacdf2ad8e8c3fdca887ca
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-18 11:58:16 -0400

    objupdater

--- obj_drills.js
+++ obj_drills.js
@@ -9,3 +9,12 @@ function createMyObject(){
         }
     };
 }
+
+
+function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+}
